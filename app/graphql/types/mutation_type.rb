5eb132f26572b90e563faeaeb56f11c8850f87db
kijima <norimaking777@gmail.com>
2022-02-01 10:16:38 +0900

    feat: add delete

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :deletePost, mutation: Mutations::DeletePost
     field :updatePost, mutation: Mutations::UpdatePost
     field :createPost, mutation: Mutations::CreatePost
   end
