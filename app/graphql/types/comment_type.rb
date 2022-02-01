48e4b1b4341e35f6fed91ab00b7f1bf5d7dafd5e
kijima <norimaking777@gmail.com>
2022-02-01 10:54:25 +0900

    feat: add create comment

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -2,12 +2,9 @@
 
 module Types
   class CommentType < Types::BaseObject
-    field :id, ID, null: false
-    field :content, String
-    field :post_id, Integer, null: false
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    description 'Comment'
     field :id, ID, null: false
     field :content, String, null: false
+    field :post, Types::PostType, null: false
   end
 end
--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -2,8 +2,23 @@
 
 module Types
   class PostType < Types::BaseObject
+    description 'Post'
+
     field :id, ID, null: false
     field :title, String, null: false
     field :description, String, null: false
+    field :comments, [Types::CommentType], null: false
+
+    # 関連を表示する
+    # {
+    #   posts {
+    #     id
+    #     title
+    #     comments {
+    #       id
+    #       content
+    #     }
+    #   }
+    # }
   end
 end
